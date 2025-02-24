dfc96dda5084c1e623bfdad43125c4b81168b236
RadicalRumin <merelenjoy66@gmail.com>
2025-02-24 14:36:01 +0100

    Initial Commit

---
+++ MovieJournal/MovieJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieJournalApp.swift
+//  MovieJournal
+//
+//  Created by Dylan on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieJournalTests/MovieJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieJournalTests.swift
+//  MovieJournalTests
+//
+//  Created by Dylan on 24/02/2025.
+//
+
+import Testing
+@testable import MovieJournal
+
+struct MovieJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
