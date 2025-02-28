5cce24d5dd734fc992148e7d996bdecbb72d8691
RadicalRumin <merelenjoy66@gmail.com>
2025-02-28 23:33:23 +0100

    Fixed CoreDatamanager

--- MovieJournal/Services/Storage/CoreDataManager.swift
+++ MovieJournal/Services/Storage/CoreDataManager.swift
@@ -12,7 +12,7 @@ class CoreDataManager {
     static let shared = CoreDataManager()
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Movie")
+        let container = NSPersistentContainer(name: "MovieModel")
         container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
